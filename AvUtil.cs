f4b1af4e8c64d0bbf07ed19529a2256b2cba1c43
Chandler Newman <chandler2newman@hotmail.co.uk>
2016-09-05 14:39:30 +0100

    Added version information to AvUtil
    Signed-off-by: Chandler Newman <chandler2newman@hotmail.co.uk>

--- AvUtil.cs
+++ AvUtil.cs
@@ -1,19 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 using FFmpeg.AutoGen;
 
 namespace FFmpeg.Wrapper
 {
     public unsafe class AvUtil
     {
+        [DllImport("avutil-55", EntryPoint = "av_version_info", CallingConvention = CallingConvention.Cdecl)]
+        private static extern char* av_version_info();
+
+        public static uint Version => ffmpeg.avutil_version();
+        public static string VersionInfo => Marshal.PtrToStringAnsi((IntPtr) av_version_info());
 
         public static VoidBuffer Malloc(ulong size)
         {
             return new VoidBuffer(ffmpeg.av_malloc(size));
         }
-
     }
 }
